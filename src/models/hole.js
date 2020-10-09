47c85e653ea94f00a3ab03272ded7a9a5c54bfe6
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2020-10-09 13:26:32 +0530

    Init commit

--- src/models/hole.js
+++ src/models/hole.js
@@ -7,7 +7,7 @@ const holeSchema = new mongoose.Schema({
     image:{type:String,required:true},
     location:{type:String,required:true,trim:true},
     district:{type:String,required:true,trim:true},
-    sender:{type:String,required:true}
+    // sender:{type:String,required:true}
 },{timestamps:true})
 const Hole = mongoose.model('Hole',holeSchema)
 module.exports=Hole
